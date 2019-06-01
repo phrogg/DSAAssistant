f06cbf57b3e3f5d9285ae3f9b14949c06d69815f
Phil Roggenbuck <ceo@roggstar.eu>
2019-06-01 10:44:05 +0200

    Release

---
+++ app/build/generated/source/buildConfig/androidTest/debug/eu/roggstar/luigithehunter/dsaassistent/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package eu.roggstar.luigithehunter.dsaassistent.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "eu.roggstar.luigithehunter.dsaassistent.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 49;
+  public static final String VERSION_NAME = "Erebor";
+}
---
+++ app/build/generated/source/buildConfig/release/eu/roggstar/luigithehunter/dsaassistent/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package eu.roggstar.luigithehunter.dsaassistent;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "eu.roggstar.luigithehunter.dsaassistent";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 49;
+  public static final String VERSION_NAME = "Erebor";
+}
